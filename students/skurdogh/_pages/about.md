c1923a78214cb699b7b3ab803f02829bb47ae40c
sarahkurd <skurdogh@calpoly.edu>
2020-04-10 01:58:09 -0700

    restarted jekyll site

---
+++ students/skurdogh/_pages/about.md
@@ -0,0 +1,8 @@
+---
+permalink: /about/
+title: "About"
+---
+
+As part of CSC 448 Bioinformatics Algorithms at Cal Poly, we will be researching and analyzing the latest novel coronavirus data on a weekly basis.
+
+This blog is one part of a larger class effort. View other students' blogs [here](https://anderson-github-classroom.github.io/csc-448-project/).
---
+++ students/skurdogh/_site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,7 @@
+var store = [{
+        "title": "An Investigation and Review of Online Coronavirus Resources",
+        "excerpt":"1) Complete Genome Sequence of a 2019 Novel Coronavirus (SARS-CoV-2) Strain Isolated in Nepal Overview A swab specimen of a Nepalese resident, who acquired the virus in Wuhan, China, and imported it to Nepal, allowed for a complete genome sequencing of a SARS-CoV-2 strain. This journal was written in early...","categories": ["research"],
+        "tags": ["corona","virus"],
+        "url": "http://localhost:4000/csc-448-project/research/Week-1-Review/",
+        "teaser": null
+      }]
